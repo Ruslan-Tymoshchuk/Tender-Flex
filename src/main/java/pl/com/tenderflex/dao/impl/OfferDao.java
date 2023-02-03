ce4cc68fe4b0b4be2b706b4620711794d213e791
R-Tim <R-Tim@DESKTOP-PTDEOCU>
2023-02-03 17:16:46 +0100

    adjusted query for offers counting

--- src/main/java/pl/com/tenderflex/dao/impl/OfferDao.java
+++ src/main/java/pl/com/tenderflex/dao/impl/OfferDao.java
@@ -14,8 +14,8 @@ public class OfferDao implements OfferRepository {
 
     public static final String ADD_NEW_OFFER_QUERY = "INSERT INTO offers(bidder_id, tender_id, organization_id, "
             + "bid_price, currency, document_name, contractor_status, bidder_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-    public static final String COUNT_OFFERS_BY_TENDER_QUERY = "SELECT count(o.id) AS aomunt_offers "
-            + "FROM offers o LEFT JOIN tenders t ON t.id = o.tender_id WHERE tender_id = ?";
+    public static final String COUNT_OFFERS_BY_TENDER_QUERY = "SELECT count(o.id) AS amount_offers "
+            + "FROM offers o WHERE tender_id = ?";
 
     private final JdbcTemplate jdbcTemplate;
     private final OrganizationDao organizationDao;
