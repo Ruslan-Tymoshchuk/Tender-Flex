5e2cadce8e7557f9c36667cb683bed0e701383e2
R-Tim <R-Tim@DESKTOP-PTDEOCU>
2023-02-24 21:56:02 +0100

    some adjustments

--- src/main/java/pl/com/tenderflex/controller/GlobalController.java
+++ src/main/java/pl/com/tenderflex/controller/GlobalController.java
@@ -1,9 +1,11 @@
 package pl.com.tenderflex.controller;
 
 import static java.util.Arrays.asList;
+import static org.springframework.http.HttpStatus.*;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import pl.com.tenderflex.model.Country;
 import pl.com.tenderflex.model.Currency;
@@ -14,16 +16,19 @@ import pl.com.tenderflex.model.TenderType;
 public class GlobalController {
 
     @GetMapping("/countries")
+    @ResponseStatus(OK)
     public List<Country> getCountries(){
         return asList(Country.values());
     }
     
     @GetMapping("/tender-types")
+    @ResponseStatus(OK)
     public List<TenderType> getTenderTypes(){
         return asList(TenderType.values());
     }
     
     @GetMapping("/currencies")
+    @ResponseStatus(OK)
     public List<Currency> getCurrencies(){
         return asList(Currency.values());
     }
