3063a1aa6f6dc8c3b915ec4174a9d7dd5b3c6f69
Ruslan Tymoshchuk <tim.ruslan14@gmail.com>
2024-05-07 02:22:06 +0200

    adjusted explanations when entered incorrect credentials by user

--- src/main/java/pl/com/tenderflex/exception/ExceptionHandlerAdvice.java
+++ src/main/java/pl/com/tenderflex/exception/ExceptionHandlerAdvice.java
@@ -19,26 +19,29 @@ public class ExceptionHandlerAdvice {
     @ExceptionHandler(BadCredentialsException.class)
     @ResponseStatus(UNAUTHORIZED)
     public ExceptionHandlerResponse handleBadCredentialsException(BadCredentialsException exception) {
-        return new ExceptionHandlerResponse(now(), UNAUTHORIZED.value(), UNAUTHORIZED, exception.getMessage(),
-                "Incorrect authentication data");
+        return new ExceptionHandlerResponse(now(), UNAUTHORIZED.value(), UNAUTHORIZED,
+                "You entered an incorrect password", "Incorrect authentication data");
     }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
-    @ResponseStatus(BAD_REQUEST)
+    @ResponseStatus(UNAUTHORIZED)
     public ExceptionHandlerResponse handleEmptyResultDataAccessException(EmptyResultDataAccessException exception) {
-        return new ExceptionHandlerResponse(now(), BAD_REQUEST.value(), BAD_REQUEST, exception.getMessage(), "Resource is not exists");
+        return new ExceptionHandlerResponse(now(), BAD_REQUEST.value(), BAD_REQUEST,
+                "The user with that email is not exists", "Resource is not exists");
     }
 
     @ExceptionHandler(DataAccessException.class)
     @ResponseStatus(BAD_REQUEST)
     public ExceptionHandlerResponse handleDataAccessException(DataAccessException exception) {
-        return new ExceptionHandlerResponse(now(), BAD_REQUEST.value(), BAD_REQUEST, exception.getMessage(), "Dao error occured");
+        return new ExceptionHandlerResponse(now(), BAD_REQUEST.value(), BAD_REQUEST, exception.getMessage(),
+                "Dao error occured");
     }
-    
+
     @ExceptionHandler(CookiesNotPresentException.class)
     @ResponseStatus(UNAUTHORIZED)
     public ExceptionHandlerResponse handleCookiesNotPresentException(CookiesNotPresentException exception) {
-        return new ExceptionHandlerResponse(now(), UNAUTHORIZED.value(), UNAUTHORIZED, exception.getMessage(), "Cookies error occured");
+        return new ExceptionHandlerResponse(now(), UNAUTHORIZED.value(), UNAUTHORIZED, exception.getMessage(),
+                "Cookies error occured");
     }
 
     @ExceptionHandler(FileNotExistsException.class)
@@ -47,21 +50,21 @@ public class ExceptionHandlerAdvice {
         return new ExceptionHandlerResponse(now(), BAD_REQUEST.value(), BAD_REQUEST, exception.getMessage(),
                 "Error occurred when uploading the file");
     }
-    
+
     @ExceptionHandler(IOException.class)
     @ResponseStatus(BAD_REQUEST)
     public ExceptionHandlerResponse handleIOException(IOException exception) {
         return new ExceptionHandlerResponse(now(), BAD_REQUEST.value(), BAD_REQUEST, exception.getMessage(),
                 "Error occurred when uploading the document");
     }
-    
+
     @ExceptionHandler(AmazonServiceException.class)
     @ResponseStatus(BAD_REQUEST)
     public ExceptionHandlerResponse handleAmazonServiceException(AmazonServiceException exception) {
         return new ExceptionHandlerResponse(now(), BAD_REQUEST.value(), BAD_REQUEST, exception.getMessage(),
                 "Error occurred when uploading the document to the backet");
     }
-    
+
     @ExceptionHandler(MissingServletRequestPartException.class)
     @ResponseStatus(BAD_REQUEST)
     public ExceptionHandlerResponse handleMissingServletRequestPartException(
