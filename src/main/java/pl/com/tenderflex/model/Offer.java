fb60d69dc7a10943026808fdbbfa26b4ee38df2a
Ruslan Tymoshchuk <tim.ruslan14@gmail.com>
2024-09-10 15:37:48 +0200

    added offer status for each user

--- src/main/java/pl/com/tenderflex/model/Offer.java
+++ src/main/java/pl/com/tenderflex/model/Offer.java
@@ -11,17 +11,14 @@ public class Offer {
 
     private Integer id;
     private User bidder;
-    private Tender tender;
     private CompanyDetails bidderCompanyDetails;
     private ContactPerson contactPerson;
     private Integer bidPrice;
     private Currency currency;
     @DateTimeFormat(pattern = "dd.MM.yyyy")
     private LocalDate publicationDate;
-    private String documentName;
-    private String awardDecision;
-    private String rejectDecision;
-    private EOfferStatus offerStatusBidder;
-    private EOfferStatus offerStatusContractor;
+    private Contract contract;
+    private OfferFile proposition;
+    private TenderFile decision;
 
 }
