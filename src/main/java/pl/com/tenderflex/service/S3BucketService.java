a25599bcf553e4278963dda56c3e8709f59cfcf1
Ruslan Tymoshchuk <tim.ruslan14@gmail.com>
2024-09-11 00:22:15 +0200

    adjustments in S3 service

--- src/main/java/pl/com/tenderflex/service/S3BucketService.java
+++ src/main/java/pl/com/tenderflex/service/S3BucketService.java
@@ -2,12 +2,11 @@ package pl.com.tenderflex.service;
 
 import java.io.IOException;
 import org.springframework.web.multipart.MultipartFile;
-
 import pl.com.tenderflex.payload.iresponse.response.MultipartFileResponse;
 
-public interface FileStorageService {
+public interface S3BucketService {
 
-    MultipartFileResponse upload(MultipartFile document, Integer userId) throws IOException;
+    String upload(MultipartFile file) throws IOException;
     
     MultipartFileResponse getPresignedUrl(String fileName);
 
--- src/main/java/pl/com/tenderflex/service/impl/S3BucketServiceImpl.java
+++ src/main/java/pl/com/tenderflex/service/impl/S3BucketServiceImpl.java
@@ -12,13 +12,11 @@ import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import pl.com.tenderflex.payload.iresponse.response.MultipartFileResponse;
-import pl.com.tenderflex.service.FileStorageService;
+import pl.com.tenderflex.service.S3BucketService;
 
 @Service
 @RequiredArgsConstructor
-public class FileStorageSeviceImpl implements FileStorageService {
-
-    public static final String LOW_LINE = "_";
+public class S3BucketServiceImpl implements S3BucketService {
 
     @Value("${bucket.name}")
     private String bucketName;
@@ -27,26 +25,23 @@ public class FileStorageSeviceImpl implements FileStorageService {
     private final AmazonS3 amazonS3Client;
 
     @Override
-    public MultipartFileResponse upload(MultipartFile document, Integer userId) throws IOException {
+    public String upload(MultipartFile file) throws IOException {
         ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentType(document.getContentType());
-        metadata.setContentLength(document.getSize());
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
         amazonS3Client.doesBucketExistV2(bucketName);
-        StringBuilder directoryKeyPrefix = new StringBuilder();
-        directoryKeyPrefix.append(randomUUID());
-        directoryKeyPrefix.append(LOW_LINE);
-        directoryKeyPrefix.append(document.getOriginalFilename());
-        amazonS3Client.putObject(bucketName, directoryKeyPrefix.toString(), document.getInputStream(), metadata);   
-        return MultipartFileResponse.builder().fileName(directoryKeyPrefix.toString()).build();
+        String fileKey = randomUUID().toString();
+        amazonS3Client.putObject(bucketName, fileKey, file.getInputStream(), metadata);
+        return fileKey;
     }
     
     @Override
-    public MultipartFileResponse getPresignedUrl(String fileName) {
+    public MultipartFileResponse getPresignedUrl(String fileKey) {
         Date expiration = new Date();
         long expTimeMillis = expiration.getTime();
         expTimeMillis += expTimePresignedUrl;
         expiration.setTime(expTimeMillis); 
-        GeneratePresignedUrlRequest presignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, fileName)
+        GeneratePresignedUrlRequest presignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, fileKey)
                 .withMethod(GET)
                 .withExpiration(expiration);
       return MultipartFileResponse.builder().fileUrl(amazonS3Client.generatePresignedUrl(presignedUrlRequest)).build();
