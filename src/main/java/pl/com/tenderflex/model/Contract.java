9ea4650d019727a1ef30dd44c8e4b7c9e4dbd490
Ruslan Tymoshchuk <tim.ruslan14@gmail.com>
2024-09-10 15:39:39 +0200

    added contract

---
+++ src/main/java/pl/com/tenderflex/model/Contract.java
@@ -0,0 +1,24 @@
+package pl.com.tenderflex.model;
+
+import java.time.LocalDate;
+import java.util.Set;
+import org.springframework.format.annotation.DateTimeFormat;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Contract {
+
+    private Integer id;
+    private Tender tender;
+    private Set<Offer>offers;
+    private Set<TenderStatus>userTenderStatuses;
+    private Set<OfferStatus>userOfferStatuses;
+    private EContractStatus status;
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
+    private LocalDate offerSubmissionDeadline;
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
+    private LocalDate signedContractDeadline;
+    
+}
--- src/main/java/pl/com/tenderflex/model/EProcedure.java
+++ src/main/java/pl/com/tenderflex/model/EProcedure.java
@@ -1,5 +1,7 @@
 package pl.com.tenderflex.model;
 
 public enum EProcedure {
+    
     OPEN_PROCEDURE
+    
 }
